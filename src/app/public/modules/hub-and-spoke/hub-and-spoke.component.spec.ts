8f91e07beddd2656ede15ab7f3138666b94fc6a2
John White <john.white@blackbaud.me>
2021-05-03 14:26:03 -0400

    Fix test

--- src/app/public/modules/hub-and-spoke/hub-and-spoke.component.spec.ts
+++ src/app/public/modules/hub-and-spoke/hub-and-spoke.component.spec.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { expect } from '@skyux-sdk/testing';
 import { SkyIconModule } from '@skyux/indicators';
 import { SkyThemeModule } from '@skyux/theme';
@@ -7,16 +9,10 @@ import { HubAndSpokeFixturesComponent } from './fixtures/hub-and-spoke-fixtures.
 import { SkyHubAndSpokeComponent } from './hub-and-spoke.component';
 
 describe('Hub and spoke component', () => {
-  /**
-   * This configureTestingModule function imports SkyAppTestModule, which brings in all of
-   * the SKY UX modules and components in your application for testing convenience. If this has
-   * an adverse effect on your test performance, you can individually bring in each of your app
-   * components and the SKY UX modules that those components rely upon.
-   */
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [HubAndSpokeFixturesComponent, SkyHubAndSpokeComponent],
-      imports: [SkyThemeModule, SkyIconModule]
+      imports: [RouterModule, CommonModule, SkyThemeModule, SkyIconModule]
     });
   });
 
