34a23db563faafb80b7c93c21149ec1c1c0fe962
John White <john.white@blackbaud.me>
2021-05-05 09:25:32 -0400

    Demo links show modal

---
+++ src/app/docs/action-hub/demo/action-hub-docs-demo.component.ts
@@ -0,0 +1,98 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { SkyConfirmService, SkyConfirmType } from '@skyux/modals';
+
+@Component({
+  selector: 'app-action-hub-docs-demo',
+  styleUrls: ['action-hub-docs-demo.component.scss'],
+  templateUrl: 'action-hub-docs-demo.component.html'
+})
+export class ActionHubDocsDemoComponent implements AfterViewInit {
+  public data = {
+    title: 'Page title',
+    needsAttention: [
+      {
+        title: '1 update',
+        message: 'from portal',
+        permalink: {
+          url: '#'
+        }
+      },
+      {
+        title: '2 new messages',
+        message: 'from online donation',
+        permalink: {
+          url: '#'
+        }
+      },
+      {
+        title: '3 possible duplicates',
+        message: 'from constituent lists',
+        permalink: {
+          url: '#'
+        }
+      }
+    ],
+    recentLinks: [
+      {
+        label: 'Recent 1',
+        permalink: {
+          url: '#'
+        }
+      },
+      {
+        label: 'Recent 2',
+        permalink: {
+          url: '#'
+        }
+      },
+      {
+        label: 'Recent 3',
+        permalink: {
+          url: '#'
+        }
+      }
+    ],
+    relatedLinks: [
+      {
+        label: 'Related 1',
+        permalink: {
+          url: '#'
+        }
+      },
+      {
+        label: 'Related 2',
+        permalink: {
+          url: '#'
+        }
+      },
+      {
+        label: 'Related 3',
+        permalink: {
+          url: '#'
+        }
+      }
+    ]
+  };
+
+  @ViewChild('actionHubComponent')
+  public actionHubComponent: ElementRef;
+
+  constructor(private confirmService: SkyConfirmService) {}
+
+  public ngAfterViewInit(): void {
+    const items = this.actionHubComponent.nativeElement.querySelectorAll('a');
+    Array.from(items).forEach((item: HTMLAnchorElement) => {
+      item.addEventListener('click', ($event: MouseEvent) => {
+        $event.preventDefault();
+        this.buttonClick($event);
+      });
+    });
+  }
+
+  public buttonClick($event: MouseEvent) {
+    this.confirmService.open({
+      message: `You pressed “${($event.currentTarget as HTMLElement).textContent.trim()}”`,
+      type: SkyConfirmType.OK
+    });
+  }
+}
